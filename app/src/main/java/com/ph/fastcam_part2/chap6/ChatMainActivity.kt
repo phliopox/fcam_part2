69dd826af7942897e1298deeaadea3675554596f
phliopox <91457591+phliopox@users.noreply.github.com>
2023-08-09 09:49:35 +0900

    bottomNavigationBar 생성, userFragment 연결

--- app/src/main/java/com/ph/fastcam_part2/chap6/ChatMainActivity.kt
+++ app/src/main/java/com/ph/fastcam_part2/chap6/ChatMainActivity.kt
@@ -5,24 +5,52 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ph.fastcam_part2.R
+import com.ph.fastcam_part2.chap6.userlist.UserFragment
 import com.ph.fastcam_part2.databinding.Chap6Binding
 
 class ChatMainActivity : AppCompatActivity() {
     private lateinit var binding: Chap6Binding
+    private val userListFragment = UserFragment()
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.chap6)
-        val currentUser = Firebase.auth.currentUser
+        binding= Chap6Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val currentUser = Firebase.auth.currentUser
         if (currentUser == null) {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
             return
         }
-    }
 
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.userList ->{
+                    replaceFragment(userListFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.chatroomList ->{
+                    return@setOnItemSelectedListener true
+                }
+                R.id.myPage ->{
+                    return@setOnItemSelectedListener true
+                }
+                else ->{
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+    }
+    private fun replaceFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.frameLayout,fragment)
+                commit()
+            }
+    }
 
 }
